102fef8d98b0eeb72aa5573c61a0eef7ac19939a
Christian Nava <navac@outlook.com>
2019-08-17 15:14:20 -0700

    Script updates before I nuke my unity

--- BlobSquad/Assets/Scripts/Projectile.cs
+++ BlobSquad/Assets/Scripts/Projectile.cs
@@ -2,16 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Projectile : MonoBehaviour
+public class Projectile : Entity
 {
-    // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public  void Update()
     {
         
     }
