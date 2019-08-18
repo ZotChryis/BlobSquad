8dc3038bd016657ca4ea606e0805d5787b94be5f
Christian Nava <navac@outlook.com>
2019-08-17 20:58:53 -0700

    castle and player changes. ui attempts

---
+++ Assets/Scripts/UIBar.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIBar : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject empty;
+
+    [SerializeField]
+    private GameObject full;
+
+    private Vector2 size = new Vector2(100, 20);
+
+    private float percent;
+
+    public void Start()
+    {
+        SetPercent(0.25f);
+    }
+
+    public void SetPercent(float percent)
+    {
+        this.percent = percent;
+        Vector3 newScale = Vector3.one;
+        newScale.x = percent;
+        full.transform.localScale = newScale;
+    }
+}
