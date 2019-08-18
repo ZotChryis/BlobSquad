bef7de7187dca34be6dca979490fb69ca747413b
Nick Grayson <ngrayson@blizzard.com>
2019-08-17 21:44:44 -0700

    WE KILLED SOMETHING FOR THE FIRST TIME!

---
+++ Assets/Scripts/Attack.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject AttackSource;
+    [SerializeField]
+    private bool FriendlyAttack;
+    public int Damage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter2D(Collider2D collider)
+    {
+        Debug.Log("Collision");
+        // Did we collide with an entity?
+        Entity entity = collider.gameObject.GetComponent<Entity>();
+        if (entity == null) return;
+
+        Debug.Log("Collision with entity");
+        bool entityAllignment = entity.isFriendly;
+        // Is this attack valid?
+        if (FriendlyAttack && !entityAllignment || !FriendlyAttack && entityAllignment)
+        {
+            Debug.Log("Gotem!");
+            entity.Wound(Damage);
+        }
+    }
+}
