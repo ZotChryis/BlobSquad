9e85ca13e591cc89d2bbaaf669c666bdc4e3ec98
Christian Nava <navac@outlook.com>
2019-08-17 23:43:44 -0700

    Added Main menu and victor screen scenes

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void OnPlayClicked()
+    {
+        SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
+    }
+
+    public void AnimationExit()
+    {
+        SceneManager.LoadScene("MainMenu", LoadSceneMode.Single);
+    }
+}
